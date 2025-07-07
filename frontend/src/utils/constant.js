3b68de686df4baaaca22c8aeedff8fe961d80d49
Bharat Patidar <bharatpatidar399@gmail.com>
2025-07-08 04:37:04 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const ADMIN_API_END_POINT = "http://localhost:8000/api/v1/admin";
-export const Employee_API_END_POINT = "http://localhost:8000/api/v1/employee";
-export const Attendance_API_END_POINT = "http://localhost:8000/api/v1/attendance";
-export const leave_API_END_POINT = "http://localhost:8000/api/v1/leave";
+export const ADMIN_API_END_POINT = "https://hosting-code-first.onrender.com/api/v1/admin";
+export const Employee_API_END_POINT = "https://hosting-code-first.onrender.com/api/v1/employee";
+export const Attendance_API_END_POINT = "https://hosting-code-first.onrender.com/api/v1/attendance";
+export const leave_API_END_POINT = "https://hosting-code-first.onrender.com/api/v1/leave";
